ae4159bb4ed9074a7187f1859006b2cf2305f5f3
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2021-10-08 22:56:39 +0200

    add example for audiosample

--- maelzel/snd/freqestimate.py
+++ maelzel/snd/freqestimate.py
@@ -2,15 +2,22 @@
 Frequency estimation of a signal with different algorithms
 """
 from __future__ import annotations
-import sys
 import numpy as np
 from scipy.signal import fftconvolve
 from scipy.signal.windows import blackmanharris
-from typing import Tuple as Tup
+import logging
+from typing import Tuple
 import bpf4
 
 
-def parabolic(f:np.ndarray, x:int) -> Tup[float, float]:
+logger = logging.getLogger(__name__)
+
+
+def _nextPow2(x: int) -> int:
+    return 1 if x == 0 else 2**(x-1).bit_length()
+
+
+def parabolic(f:np.ndarray, x:int) -> Tuple[float, float]:
     """
     Quadratic interpolation for estimating the true position of an
     inter-sample maximum when nearby samples are known.
@@ -46,7 +53,7 @@ def find(condition) -> np.ndarray:
     return res
 
 
-def freq_from_crossings(sig:np.ndarray, sr:int) -> float:
+def f0ViaZeroCrossings(sig:np.ndarray, sr:int) -> Tuple[float, float]:
     """Estimate frequency by counting zero crossings
 
     Args:
@@ -56,6 +63,9 @@ def freq_from_crossings(sig:np.ndarray, sr:int) -> float:
     Returns:
         the frequency of the signal
     """
+    if _sigNumChannels(sig) > 1:
+        raise ValueError("sig should be a mono signal")
+
     # Find all indices right before a rising-edge zero crossing
     indices = find((sig[1:] >= 0) & (sig[:-1] < 0))
     
@@ -68,19 +78,22 @@ def freq_from_crossings(sig:np.ndarray, sr:int) -> float:
     
     # Some other interpolation based on neighboring points might be better. 
     # Spline, cubic, whatever
-    return sr / np.mean(np.diff(crossings))
+    return sr / np.mean(np.diff(crossings)), 1
 
 
-def freq_from_fft(sig:np.ndarray, sr:int) -> float:
+def f0ViaFFT(sig:np.ndarray, sr:int) -> Tuple[float, float]:
     """Estimate frequency from peak of FFT
 
     Args:
-        sig: a sampled signal
+        sig: a sampled signal (mono)
         sr : sample rate
 
     Returns:
-        the frequency of the signal
+        the frequency of the signal, the probability that the sound
+        frequency is valid
     """
+    if _sigNumChannels(sig) > 1:
+        raise ValueError("sig should be a mono signal")
     # Compute Fourier transform of windowed signal
     windowed = sig * blackmanharris(len(sig))
     f = np.fft.rfft(windowed)
@@ -90,10 +103,10 @@ def freq_from_fft(sig:np.ndarray, sr:int) -> float:
     true_i = parabolic(np.log(abs(f)), i)[0]
     
     # Convert to equivalent frequency
-    return sr * true_i / len(windowed)
+    return sr * true_i / len(windowed), 1
 
 
-def freq_from_autocorr(sig:np.ndarray, sr:int) -> float:
+def f0ViaAutocorrelation(sig:np.ndarray, sr:int) -> Tuple[float, float]:
     """
     Estimate frequency using autocorrelation
 
@@ -102,8 +115,12 @@ def freq_from_autocorr(sig:np.ndarray, sr:int) -> float:
         sr : sample rate
 
     Returns:
-        the frequency of the signal
+        the frequency of the signal, the probability that the sound
+        frequency is valid
     """
+    if _sigNumChannels(sig) > 1:
+        raise ValueError("sig should be a mono signal")
+
     # Calculate autocorrelation (same thing as convolution, but with 
     # one input reversed in time), and throw away the negative lags
     corr = fftconvolve(sig, sig[::-1], mode='full')
@@ -119,14 +136,17 @@ def freq_from_autocorr(sig:np.ndarray, sr:int) -> float:
     # Should use a weighting function to de-emphasize the peaks at longer lags.
     peak = np.argmax(corr[start:]) + start
     px, py = parabolic(corr, peak) 
-    return sr / px
+    return sr / px, 1
 
 
-def freq_from_HPS(sig:np.ndarray, sr:int, maxharms=5) -> float:
+def f0ViaHPS(sig:np.ndarray, sr:int, maxharms=5) -> Tuple[float, float]:
     """
     Estimate frequency using harmonic product spectrum (HPS)
     
     """
+    if _sigNumChannels(sig) > 1:
+        raise ValueError("sig should be a mono signal")
+
     windowed = sig * blackmanharris(len(sig))
     c = abs(np.fft.rfft(windowed))
     freq = 0
@@ -138,44 +158,129 @@ def freq_from_HPS(sig:np.ndarray, sr:int, maxharms=5) -> float:
         try:
             true_i = parabolic(abs(c), i)[0]
         except IndexError:
-            return freq
+            return freq, 0
         freq = sr * true_i / len(windowed)
-        print('Pass %d: %f Hz' % (x, freq))
         c *= a
-    return freq
+    return freq, 1
 
 
-def frequency_bpf(sig:np.ndarray, sr:int, lowestfreq=100, steptime=0.01,
-                  method='autocorrelation'
-                  ) -> bpf4.BpfInterface:
-    windowsize = int(1/lowestfreq * sr)
-    stepsize = int(steptime * sr)
+def f0curvePyin(sig: np.ndarray, sr:int, minfreq=50, maxfreq=5000,
+                framelength=2048, winlength=None, hoplength=512
+                ) -> Tuple[bpf4.BpfInterface, bpf4.BpfInterface]:
+    """
+    Calculate the fundamental based on the pyin method
+
+    This routine is implemented in pure python and is VERY slow
+
+    Args:
+        sig: the array representing the soud
+        sr: sample rate
+        minfreq: lowest frequency to considere for f0
+        maxfreq: highest frequecy to considere for f0
+        framelength: the fft size
+        winlength: the window length (must be smaller than framelength)
+        hoplength: interval between measurements (in samples)
+
+    Returns:
+        a tuple (freq curve, voiced probability curve)
+
+    """
+    if _sigNumChannels(sig) > 1:
+        raise ValueError("sig should be a mono signal")
+
+    import librosa
+    totaldur = len(sig)/sr
+    f0, voiced_flag, voiced_probs = librosa.pyin(sig, sr=sr,
+                                                 fmin=minfreq, fmax=maxfreq,
+                                                 frame_length=framelength,
+                                                 win_length=winlength,
+                                                 hop_length=hoplength)
+    times = np.linspace(0, totaldur, len(f0))
+    return bpf4.core.Linear(times, f0), bpf4.core.Linear(times, voiced_probs)
+
+
+def _fftWinlength(sr: int, minfreq: int) -> int:
+    return _nextPow2(int(sr/minfreq))
+
+def f0curvePyinVamp(sig: np.ndarray, sr:int, fftsize=2048, overlap=4,
+                    lowAmpSupression=0.1, onsetSensitivity=0.7,
+                    pruneThreshold=0.1,
+                    threshDistr='beta15'
+
+                    ) -> Tuple[bpf4.BpfInterface, bpf4.BpfInterface]:
+    """
+    Calculate the fundamental using 'vamptools' and the pyin vamp plugin
+
+    Args:
+        sig: the signal as numpy array
+        sr: the samplerate
+        fftsize: with sizes greater than 2048 the result might be unstable
+        overlap: hop size as fftsize//overlap
+        lowAmpSupression: ??
+        onsetSensitivity: ??
+        pruneThreshold: ??
+        threshDistr: one of beta10, beta15, beta20, beta30
+
+    Returns:
+        a tuple (f0 bpf, probability bpf), where f0 is a bpf with the
+        detected fundamental. Whenver the algorithms detects unvoiced
+        (noise) or absence of a fundamental, the result is negative.
+        
+    """
+    if _sigNumChannels(sig) > 1:
+        raise ValueError("sig should be a mono signal")
+
+    from maelzel.snd import vamptools
+    data = vamptools.pyin_pitchtrack(sig, sr=sr, fft_size=fftsize, overlap=overlap,
+                                     low_amp_suppression=lowAmpSupression,
+                                     onset_sensitivity=onsetSensitivity,
+                                     prune_thresh=pruneThreshold,
+                                     thresh_distr=threshDistr)
+    times = data[:, 0]
+    f0 = data[:, 1]
+    probs = data[:, 2]
+    return bpf4.core.Linear(times, f0), bpf4.core.Linear(times, probs)
+
+
+def _sigNumChannels(sig: np.ndarray):
+    return sig.shape[1] if len(sig.shape) > 1 else 1
+
+
+def f0curve(sig:np.ndarray, sr:int, minfreq=100, steptime=0.01,
+            method='pyin-librosa'
+            ) -> Tuple[bpf4.BpfInterface, bpf4.BpfInterface]:
+    if _sigNumChannels(sig) > 1:
+        raise ValueError("sig should be a mono signal")
+
+    if method == 'pyin-librosa':
+        hoplength = int(steptime * sr)
+        winlength = _fftWinlength(sr, minfreq)
+        return f0curvePyin(sig, sr, minfreq=minfreq,
+                           framelength=winlength*2,
+                           winlength=winlength, hoplength=hoplength)
+    elif method == 'pyin-vamp':
+        for f0, f1, fftsize in [(0,   30, 8192),
+                                (30,  60, 4096),
+                                (60, 600, 2048),
+                                (600, 99999, 1024)]:
+            if f0 <= minfreq < f1:
+                break
+        return f0curvePyinVamp(sig, sr, fftsize=fftsize)
+    stepsize = int(steptime*sr)
+    windowsize = _fftWinlength(sr, minfreq)
     maxidx = len(sig) - windowsize
     maxn = int(maxidx / stepsize)
     func = {
-        'autocorrelation': freq_from_autocorr,
-        'fft': freq_from_fft,
-        'hps': freq_from_HPS
+        'autocorrelation': f0ViaAutocorrelation,
+        'fft': f0ViaFFT,
+        'hps': f0ViaHPS
     }[method]
-    freqs = []
-    times = []
+    freqs, times, probs = [], [], []
     for n in range(maxn):
         idx = n * stepsize
         arr = sig[idx: idx+windowsize]
-        freq = func(arr, sr)
+        freq, prob = func(arr, sr)
         freqs.append(freq)
         times.append(idx/sr)
-    return bpf4.core.Linear(times, freqs)
-
-
-if __name__ == '__main__':
-    filename = sys.argv[1]
-    
-    print('Reading file "%s"\n' % filename)
-    import sndfileio
-    signal, sr = sndfileio.sndread(filename)
-
-    print('Calculating freq from FFT: %f Hz' % freq_from_fft(signal, sr))
-    print('Calculating freq from zerocross: %f Hz: ' % freq_from_crossings(signal, sr))
-    print('Calculating freq from autocorrelation: %f Hz' % freq_from_autocorr(signal, sr))
-    print('Calculating freq from HPS: %f Hz' % freq_from_HPS(signal, sr))
+        probs.append(prob)
+    return bpf4.core.Linear(times, freqs), bpf4.core.Linear(times, probs)
